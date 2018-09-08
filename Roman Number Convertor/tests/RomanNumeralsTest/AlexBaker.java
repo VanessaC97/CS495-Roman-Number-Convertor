aa74a9638ea95fa5022016ea1a66af2f642bc14e
Alex Baker <bakerab@sbcglobal.net>
2018-09-08 17:45:10 -0400

    Add more test cases for error handling

--- Roman Number Convertor/tests/RomanNumeralsTest/AlexBaker.java
+++ Roman Number Convertor/tests/RomanNumeralsTest/AlexBaker.java
@@ -15,6 +15,26 @@ public class AlexBaker {
         }
         Assertions.assertEquals("XLVII", numerals);
     }
+    @Test
+    public void ToNumber_InvalidNumber() {
+        Exception output = null;
+        try {
+            RomanNumeralConverter.ConvertToRomanNumbers(0);
+        } catch (RomanError e) {
+            output = e;
+        }
+        Assert.assertNotEquals(null, output);
+    }
 
+    @Test
+    public void ToNumber_One() {
+        String numerals = null;
+        try {
+            numerals = RomanNumeralConverter.ConvertToRomanNumbers(1);
+        } catch (RomanError e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals("I", numerals);
+    }
 
 }
